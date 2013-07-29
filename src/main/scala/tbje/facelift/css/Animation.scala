b9a4e5c689681828ad34c8883420837c5136cc1a
Trond Bjerkestrand <trond.bjerkestrand@typesafe.com>
2013-07-29 11:18:19 +0200

    Adding stubs and TODOs for Animation.

---
+++ src/main/scala/tbje/facelift/css/Animation.scala
@@ -0,0 +1,55 @@
+package tbje.facelift.css
+
+/**
+ * Specifies the animation
+ * Available from CSS version 3
+ */
+// TODO: @keyframes
+
+/**
+ * A shorthand property for all the animation properties below, except the animation-play-state property
+ * Available from CSS version 3
+ */
+// TODO: animation
+
+/**
+ * Specifies a name for the @keyframes animation
+ * Available from CSS version 3
+ */
+// TODO: animation-name
+
+/**
+ * Specifies how many seconds or milliseconds an animation takes to complete one cycle
+ * Available from CSS version 3
+ */
+// TODO: animation-duration
+
+/**
+ * Specifies the speed curve of the animation
+ * Available from CSS version 3
+ */
+// TODO: animation-timing-function
+
+/**
+ * Specifies when the animation will start
+ * Available from CSS version 3
+ */
+// TODO: animation-delay
+
+/**
+ * Specifies  the number of times an animation should be played
+ * Available from CSS version 3
+ */
+// TODO: animation-iteration-count
+
+/**
+ * Specifies whether or not the animation should play in reverse on alternate cycles
+ * Available from CSS version 3
+ */
+// TODO: animation-direction
+
+/**
+ * Specifies whether the animation is running or paused
+ * Available from CSS version 3
+ */
+// TODO: animation-play-state
