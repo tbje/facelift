ee8a11b623e8dffefe865354561849c4ebb2d722
Trond Bjerkestrand <trond.bjerkestrand@typesafe.com>
2013-07-30 14:36:46 +0200

    Adding font-weight and some font TODOs.

--- src/main/scala/tbje/facelift/css/Font.scala
+++ src/main/scala/tbje/facelift/css/Font.scala
@@ -91,3 +91,65 @@ object FontSize {
 case class FontSizeEm(size: Double) extends FontSize(s"${size}em")
 
 case class FontSizePx(size: Int) extends FontSize(s"${size}px")
+
+/**
+ * Sets all the font properties in one declaration
+ * Available from CSS version 1
+ */
+// TODO: font
+
+/**
+ * Specifies the font style for text
+ * Available from CSS version 1
+ */
+// TODO: font-style
+
+/**
+ * Specifies whether or not a text should be displayed in a small-caps font
+ * Available
+ */
+// TODO: font-variant
+
+/**
+ * Specifies the weight of a font
+ * Available from CSS version 1
+ */
+class FontWeight(val value: String) extends CssDeclaration {
+  val property = "font-weight"
+}
+
+object FontWeight {
+  def apply(value: String) = new FontWeight(value)
+  def apply(value: Int) = new FontWeight(value.toString)
+  val Weight100 = FontWeight(100)
+  val Weight200 = FontWeight(200)
+  val Weight300 = FontWeight(300)
+  val Weight400 = FontWeight(400)
+  val Weight500 = FontWeight(500)
+  val Weight600 = FontWeight(600)
+  val Weight700 = FontWeight(700)
+  val Weight800 = FontWeight(800)
+  val Weight900 = FontWeight(900)
+  val Normal = FontWeight("normal") // Defines normal characters. This is default
+  val Bold = FontWeight("bold") // Defines thick characters
+  val Bolder = FontWeight("bolder") // Defines thicker characters
+  val Lighter = FontWeight("lighter") // Defines lighter characters
+}
+
+/**
+ * A rule that allows websites to download and use fonts other than the "web-safe" fonts
+ * Available from CSS version 3
+ */
+// TODO: @font-face
+
+/**
+ * Preserves the readability of text when font fallback occurs
+ * Available from CSS version 3
+ */
+// TODO: font-size-adjust
+
+/**
+ * Selects a normal, condensed, or expanded face from a font family
+ * Available from CSS version 3
+ */
+// TODO: font-stretch
